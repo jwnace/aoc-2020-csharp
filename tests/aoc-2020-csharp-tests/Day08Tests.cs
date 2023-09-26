0b2d18e409b8cb70011ff0cb476d60bf8bb04632
Joe Nace <jwnace@gmail.com>
2023-09-26 00:03:51 -0400

    added boiler plate and placeholders for solutions

---
+++ tests/aoc-2020-csharp-tests/Day08Tests.cs
@@ -0,0 +1,22 @@
+﻿using aoc_2020_csharp.Day08;
+
+namespace aoc_2020_csharp_tests;
+
+public class Day08Tests
+{
+    [Test]
+    public void Part1_ReturnsCorrectAnswer()
+    {
+        var expected = 0;
+        var actual = Day08.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_ReturnsCorrectAnswer()
+    {
+        var expected = 0;
+        var actual = Day08.Part2();
+        actual.Should().Be(expected);
+    }
+}
