5765e8e5c4f7fa8f25435c76eb00fa66b7ec30d4
Joe Nace <jwnace@gmail.com>
2023-09-26 16:46:30 -0400

    initial solution for day 01

--- src/aoc-2020-csharp/Day01/Day01.cs
+++ src/aoc-2020-csharp/Day01/Day01.cs
@@ -2,9 +2,44 @@
 
 public static class Day01
 {
-    private static readonly string Input = File.ReadAllText("Day01/day01.txt");
+    private static readonly int[] Input = File.ReadAllLines("Day01/day01.txt").Select(int.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(int[] input)
+    {
+        foreach (var a in input)
+        {
+            foreach (var b in input)
+            {
+                if (a + b == 2020)
+                {
+                    return a * b;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public static int Solve2(int[] input)
+    {
+        foreach (var a in input)
+        {
+            foreach (var b in input)
+            {
+                foreach (var c in input)
+                {
+                    if (a + b + c == 2020)
+                    {
+                        return a * b * c;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
 }
--- tests/aoc-2020-csharp-tests/Day01Tests.cs
+++ tests/aoc-2020-csharp-tests/Day01Tests.cs
@@ -5,17 +5,35 @@ namespace aoc_2020_csharp_tests;
 public class Day01Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { 1721, 979, 366, 299, 675, 1456 };
+        var expected = 514579;
+        var actual = Day01.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 1010299;
         var actual = Day01.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { 1721, 979, 366, 299, 675, 1456 };
+        var expected = 241861950;
+        var actual = Day01.Solve2(input);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 42140160;
         var actual = Day01.Part2();
         actual.Should().Be(expected);
     }
