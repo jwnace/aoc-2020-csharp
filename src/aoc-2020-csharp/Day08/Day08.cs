572a08dfa32c7c24369e9f988706a13779979127
Joe Nace <jwnace@gmail.com>
2023-09-27 19:45:19 -0400

    added solution for day 08

--- src/aoc-2020-csharp/Day08/Day08.cs
+++ src/aoc-2020-csharp/Day08/Day08.cs
@@ -8,13 +8,80 @@ public static class Day08
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string[] input)
-    {
-        throw new NotImplementedException();
-    }
+    public static int Solve1(string[] input) =>
+        RunProgram(input, out var accumulator) ? throw new Exception("Program halted") : accumulator;
 
     public static int Solve2(string[] input)
     {
-        throw new NotImplementedException();
+        var indexToModify = 0;
+
+        while (true)
+        {
+            var program = ModifyProgram(input, ref indexToModify);
+            var halts = RunProgram(program, out var accumulator);
+
+            if (halts)
+            {
+                return accumulator;
+            }
+
+            indexToModify++;
+        }
+    }
+
+    private static string[] ModifyProgram(string[] input, ref int indexToModify)
+    {
+        var program = input.ToArray();
+
+        switch (program[indexToModify][..3])
+        {
+            case "nop":
+                program[indexToModify] = program[indexToModify].Replace("nop", "jmp");
+                break;
+            case "jmp":
+                program[indexToModify] = program[indexToModify].Replace("jmp", "nop");
+                break;
+            default:
+                indexToModify++;
+                break;
+        }
+
+        return program;
+    }
+
+    private static bool RunProgram(string[] input, out int accumulator)
+    {
+        accumulator = 0;
+        var visited = new HashSet<int>();
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            if (visited.Contains(i))
+            {
+                return false;
+            }
+
+            visited.Add(i);
+
+            var instruction = input[i];
+            var parts = instruction.Split(" ");
+            var (op, arg) = (parts[0], int.Parse(parts[1]));
+
+            switch (op)
+            {
+                case "nop":
+                    break;
+                case "acc":
+                    accumulator += arg;
+                    break;
+                case "jmp":
+                    i += arg - 1;
+                    break;
+                default:
+                    throw new Exception($"Unknown instruction: {instruction}");
+            }
+        }
+
+        return true;
     }
 }
