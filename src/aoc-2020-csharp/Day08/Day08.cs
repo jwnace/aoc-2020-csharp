687efd4db5fb14f786d8eb1fcfed1f5a1075620d
Joe Nace <jwnace@gmail.com>
2023-09-27 18:02:23 -0400

    added tests for day 08

--- src/aoc-2020-csharp/Day08/Day08.cs
+++ src/aoc-2020-csharp/Day08/Day08.cs
@@ -2,9 +2,19 @@ namespace aoc_2020_csharp.Day08;
 
 public static class Day08
 {
-    private static readonly string Input = File.ReadAllText("Day08/day08.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day08/day08.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int Solve2(string[] input)
+    {
+        throw new NotImplementedException();
+    }
 }
