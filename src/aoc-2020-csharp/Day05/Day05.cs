697db92dc02725365864e86398f1ef43cfb9bfeb
Joe Nace <jwnace@gmail.com>
2023-09-27 14:24:46 -0400

    added solution for day 05

--- src/aoc-2020-csharp/Day05/Day05.cs
+++ src/aoc-2020-csharp/Day05/Day05.cs
@@ -1,10 +1,40 @@
-﻿namespace aoc_2020_csharp.Day05;
+﻿using aoc_2020_csharp.Extensions;
+
+namespace aoc_2020_csharp.Day05;
 
 public static class Day05
 {
-    private static readonly string Input = File.ReadAllText("Day05/day05.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day05/day05.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => Input.Select(GetSeatId).Max();
 
-    public static int Part2() => 2;
+    public static int Part2() => Input.Select(GetSeatId)
+        .OrderBy(x => x)
+        .Windowed(2)
+        .Where(x => x[1] - x[0] == 2)
+        .Select(x => x[0] + 1)
+        .Single();
+
+    public static int GetSeatId(string input) => GetRow(input[..7]) * 8 + GetColumn(input[7..]);
+
+    private static int GetRow(string input) => FindPartition(input, Enumerable.Range(0, 128).ToArray());
+
+    private static int GetColumn(string input) => FindPartition(input, Enumerable.Range(0, 8).ToArray());
+
+    private static int FindPartition(string input, int[] range)
+    {
+        foreach (var c in input)
+        {
+            var half = range.Length / 2;
+
+            range = c switch
+            {
+                'F' or 'L' => range.Take(half).ToArray(),
+                'B' or 'R' => range.Skip(half).ToArray(),
+                _ => throw new ArgumentException($"Invalid character: {c}")
+            };
+        }
+
+        return range.Single();
+    }
 }
--- tests/aoc-2020-csharp-tests/Day05Tests.cs
+++ tests/aoc-2020-csharp-tests/Day05Tests.cs
@@ -4,10 +4,20 @@ namespace aoc_2020_csharp_tests;
 
 public class Day05Tests
 {
+    [TestCase("FBFBBFFRLR", 357)]
+    [TestCase("BFFFBBFRRR", 567)]
+    [TestCase("FFFBBBFRRR", 119)]
+    [TestCase("BBFFBBFRLL", 820)]
+    public void GetSeatId_ReturnsCorrectAnswer(string input, int expected)
+    {
+        var actual = Day05.GetSeatId(input);
+        actual.Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 892;
         var actual = Day05.Part1();
         actual.Should().Be(expected);
     }
@@ -15,7 +25,7 @@ public class Day05Tests
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 625;
         var actual = Day05.Part2();
         actual.Should().Be(expected);
     }
