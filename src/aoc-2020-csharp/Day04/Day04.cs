01a257322867d38e90e28875308d8cd6160b111b
Joe Nace <jwnace@gmail.com>
2023-09-27 09:28:00 -0400

    added solution for part 1

--- src/aoc-2020-csharp/Day04/Day04.cs
+++ src/aoc-2020-csharp/Day04/Day04.cs
@@ -2,9 +2,63 @@
 
 public static class Day04
 {
-    private static readonly string Input = File.ReadAllText("Day04/day04.txt");
+    private static readonly string Input = File.ReadAllText("Day04/day04.txt").Trim();
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(string input) =>
+        input.Split($"{Environment.NewLine + Environment.NewLine}").Select(Passport.Parse).Count(p => p.IsValid());
+
+    public static int Solve2(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private record Passport(
+        string? BirthYear,
+        string? IssueYear,
+        string? ExpirationYear,
+        string? Height,
+        string? HairColor,
+        string? EyeColor,
+        string? PassportId,
+        string? CountryId)
+    {
+        public static Passport Parse(string input)
+        {
+            var dict = new Dictionary<string, string>();
+            var pairs = input.Split(null);
+
+            foreach (var pair in pairs)
+            {
+                var parts = pair.Split(":");
+                var (key, value) = (parts[0], parts[1]);
+
+                dict[key] = value;
+            }
+
+            return new Passport(
+                dict.GetValueOrDefault("byr"),
+                dict.GetValueOrDefault("iyr"),
+                dict.GetValueOrDefault("eyr"),
+                dict.GetValueOrDefault("hgt"),
+                dict.GetValueOrDefault("hcl"),
+                dict.GetValueOrDefault("ecl"),
+                dict.GetValueOrDefault("pid"),
+                dict.GetValueOrDefault("cid"));
+        }
+
+        public bool IsValid()
+        {
+            return BirthYear != null &&
+                   IssueYear != null &&
+                   ExpirationYear != null &&
+                   Height != null &&
+                   HairColor != null &&
+                   EyeColor != null &&
+                   PassportId != null;
+        }
+    }
 }
