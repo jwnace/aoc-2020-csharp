a01fa64a118e7feb5106a1b27f7e54f62f00b0ba
Joe Nace <jwnace@gmail.com>
2023-09-27 23:01:32 -0400

    added solution for day 10

--- src/aoc-2020-csharp/Day10/Day10.cs
+++ src/aoc-2020-csharp/Day10/Day10.cs
@@ -1,10 +1,38 @@
-﻿namespace aoc_2020_csharp.Day10;
+﻿using aoc_2020_csharp.Extensions;
+
+namespace aoc_2020_csharp.Day10;
 
 public static class Day10
 {
-    private static readonly string Input = File.ReadAllText("Day10/day10.txt");
+    private static readonly int[] Input = File.ReadAllLines("Day10/day10.txt").Select(int.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static long Part2() => Solve2(Input);
+
+    public static int Solve1(int[] input)
+    {
+        var dictionary = input
+            .OrderBy(x => x)
+            .Prepend(0)
+            .Append(input.Max() + 3)
+            .Windowed(2)
+            .GroupBy(x => x[1] - x[0])
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        return dictionary[1] * dictionary[3];
+    }
+
+    public static long Solve2(int[] input)
+    {
+        var memo = new Dictionary<int, long> { { input.Max() + 3, 1 } };
+        var adapters = input.OrderByDescending(x => x).Append(0);
+
+        foreach (var adapter in adapters)
+        {
+            memo[adapter] = Enumerable.Range(1, 3).Select(i => memo.GetValueOrDefault(adapter + i, 0L)).Sum();
+        }
+
+        return memo[0];
+    }
 }
--- tests/aoc-2020-csharp-tests/Day10Tests.cs
+++ tests/aoc-2020-csharp-tests/Day10Tests.cs
@@ -4,18 +4,34 @@ namespace aoc_2020_csharp_tests;
 
 public class Day10Tests
 {
+    [TestCase(new[] { 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, }, 35)]
+    [TestCase(new[] { 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3 }, 220)]
+    public void Part1_Example_ReturnsCorrectAnswer(int[] input, int expected)
+    {
+        var actual = Day10.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2059;
         var actual = Day10.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [TestCase(new[] { 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4, }, 8)]
+    [TestCase(new[] { 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3 }, 19208)]
+    public void Part2_Example_ReturnsCorrectAnswer(int[] input, int expected)
+    {
+        var actual = Day10.Solve2(input);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 86812553324672;
         var actual = Day10.Part2();
         actual.Should().Be(expected);
     }
