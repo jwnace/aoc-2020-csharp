eb3bd00ddbfd03f8d6a1c74cd012660e83b7cf94
Joe Nace <jwnace@gmail.com>
2023-09-28 11:58:30 -0400

    added tests for day 14

--- src/aoc-2020-csharp/Day14/Day14.cs
+++ src/aoc-2020-csharp/Day14/Day14.cs
@@ -2,9 +2,19 @@
 
 public static class Day14
 {
-    private static readonly string Input = File.ReadAllText("Day14/day14.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day14/day14.txt");
 
-    public static int Part1() => 1;
+    public static long Part1() => 1;
 
-    public static int Part2() => 2;
+    public static long Part2() => 2;
+
+    public static long Solve1(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static long Solve2(string[] input)
+    {
+        throw new NotImplementedException();
+    }
 }
