4939c70f9c7ae8c70a8c1a89dace87161bcb8c6b
Joe Nace <jwnace@gmail.com>
2023-09-28 11:52:09 -0400

    added solution for day 13

--- tests/aoc-2020-csharp-tests/Day13Tests.cs
+++ tests/aoc-2020-csharp-tests/Day13Tests.cs
@@ -7,12 +7,7 @@ public class Day13Tests
     [Test]
     public void Part1_Example_ReturnsCorrectAnswer()
     {
-        var input =
-            """
-            939
-            7,13,x,x,59,x,31,19
-            """;
-
+        var input = new[] { "939", "7,13,x,x,59,x,31,19" };
         var expected = 295;
         var actual = Day13.Solve1(input);
         actual.Should().Be(expected);
@@ -26,13 +21,13 @@ public class Day13Tests
         actual.Should().Be(expected);
     }
 
-    [TestCase("7,13,x,x,59,x,31,19", 1068781)]
-    [TestCase("17,x,13,19", 3417)]
-    [TestCase("67,7,59,61", 754018)]
-    [TestCase("67,x,7,59,61", 779210)]
-    [TestCase("67,7,x,59,61", 1261476)]
-    [TestCase("1789,37,47,1889", 1202161486)]
-    public void Part2_Example_ReturnsCorrectAnswer(string input, long expected)
+    [TestCase(new[] { "939", "7,13,x,x,59,x,31,19" }, 1068781)]
+    [TestCase(new[] { "939", "17,x,13,19" }, 3417)]
+    [TestCase(new[] { "939", "67,7,59,61" }, 754018)]
+    [TestCase(new[] { "939", "67,x,7,59,61" }, 779210)]
+    [TestCase(new[] { "939", "67,7,x,59,61" }, 1261476)]
+    [TestCase(new[] { "939", "1789,37,47,1889" }, 1202161486)]
+    public void Part2_Example_ReturnsCorrectAnswer(string[] input, long expected)
     {
         var actual = Day13.Solve2(input);
         actual.Should().Be(expected);
