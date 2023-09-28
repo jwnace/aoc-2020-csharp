71db6a0d3bf3c9ead31d960ea2b87bc94ea4208b
Joe Nace <jwnace@gmail.com>
2023-09-28 00:05:57 -0400

    added solution for part 1

--- src/aoc-2020-csharp/Day11/Day11.cs
+++ src/aoc-2020-csharp/Day11/Day11.cs
@@ -2,9 +2,80 @@
 
 public static class Day11
 {
-    private static readonly string Input = File.ReadAllText("Day11/day11.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day11/day11.txt");
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(string[] input)
+    {
+        var grid = BuildGrid(input);
+
+        while (true)
+        {
+            var newGrid = new Dictionary<(int Row, int Col), char>(grid);
+
+            foreach (var seat in grid)
+            {
+                var (row, col) = seat.Key;
+                var occupiedNeighbors = CountOccupiedNeighbors(row, col, grid);
+
+                newGrid[seat.Key] = seat.Value switch
+                {
+                    'L' when occupiedNeighbors == 0 => '#',
+                    '#' when occupiedNeighbors >= 4 => 'L',
+                    _ => newGrid[seat.Key]
+                };
+            }
+
+            if (grid.SequenceEqual(newGrid))
+            {
+                break;
+            }
+
+            grid = newGrid;
+        }
+
+        return grid.Count(s => s.Value == '#');
+    }
+
+    public static int Solve2(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Dictionary<(int Row, int Col), char> BuildGrid(string[] input)
+    {
+        var grid = new Dictionary<(int Row, int Col), char>();
+
+        for (var row = 0; row < input.Length; row++)
+        {
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                if (input[row][col] == 'L')
+                {
+                    grid[(row, col)] = 'L';
+                }
+            }
+        }
+
+        return grid;
+    }
+
+    private static int CountOccupiedNeighbors(int row, int col, Dictionary<(int Row, int Col), char> grid) =>
+        GetNeighbors(row, col).Count(n => grid.TryGetValue(n, out var v) && v == '#');
+
+    private static (int, int)[] GetNeighbors(int row, int col) =>
+        new[]
+        {
+            (row - 1, col - 1),
+            (row - 1, col),
+            (row - 1, col + 1),
+            (row, col - 1),
+            (row, col + 1),
+            (row + 1, col - 1),
+            (row + 1, col),
+            (row + 1, col + 1),
+        };
 }
--- tests/aoc-2020-csharp-tests/Day11Tests.cs
+++ tests/aoc-2020-csharp-tests/Day11Tests.cs
@@ -5,17 +5,61 @@ namespace aoc_2020_csharp_tests;
 public class Day11Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL",
+        };
+
+        var expected = 37;
+        var actual = Day11.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2310;
         var actual = Day11.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL",
+        };
+
+        var expected = 26;
+        var actual = Day11.Solve2(input);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2074;
         var actual = Day11.Part2();
         actual.Should().Be(expected);
     }
