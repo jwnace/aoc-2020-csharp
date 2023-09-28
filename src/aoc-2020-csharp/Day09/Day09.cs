c109bd495c14dfd275f14c06a5b5b50999e3bbc2
Joe Nace <jwnace@gmail.com>
2023-09-27 20:49:24 -0400

    added solution for day 09

--- src/aoc-2020-csharp/Day09/Day09.cs
+++ src/aoc-2020-csharp/Day09/Day09.cs
@@ -1,10 +1,58 @@
+using aoc_2020_csharp.Extensions;
+
 namespace aoc_2020_csharp.Day09;
 
 public static class Day09
 {
-    private static readonly string Input = File.ReadAllText("Day09/day09.txt");
+    private static readonly long[] Input = File.ReadAllLines("Day09/day09.txt").Select(long.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static long Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static long Part2() => Solve2(Input);
+
+    public static long Solve1(long[] input, int preambleLength = 25)
+    {
+        var windows = input.Windowed(preambleLength + 1);
+
+        foreach (var window in windows)
+        {
+            var preamble = window[..preambleLength];
+            var target = window[^1];
+
+            if (preamble.Any(x => preamble.Contains(target - x)))
+            {
+                continue;
+            }
+
+            return target;
+        }
+
+        throw new Exception("No solution found");
+    }
+
+    public static long Solve2(long[] input, int preambleLength = 25)
+    {
+        var target = Solve1(input, preambleLength);
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            for (var j = i + 1; j < input.Length; j++)
+            {
+                var range = input[i..j];
+                var sum = range.Sum();
+
+                if (sum == target)
+                {
+                    return range.Min() + range.Max();
+                }
+
+                if (sum > target)
+                {
+                    break;
+                }
+            }
+        }
+
+        throw new Exception("No solution found");
+    }
 }
--- tests/aoc-2020-csharp-tests/Day09Tests.cs
+++ tests/aoc-2020-csharp-tests/Day09Tests.cs
@@ -5,17 +5,35 @@ namespace aoc_2020_csharp_tests;
 public class Day09Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { 35L, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576 };
+        var expected = 127L;
+        var actual = Day09.Solve1(input, 5);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 88311122L;
         var actual = Day09.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { 35L, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576 };
+        var expected = 62L;
+        var actual = Day09.Solve2(input, 5);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 13549369;
         var actual = Day09.Part2();
         actual.Should().Be(expected);
     }
