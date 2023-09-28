5ba2c5ac6bbfca9042ff98d88ed6c80862d864b8
Joe Nace <jwnace@gmail.com>
2023-09-28 01:45:31 -0400

    added tests for day 12

--- src/aoc-2020-csharp/Day12/Day12.cs
+++ src/aoc-2020-csharp/Day12/Day12.cs
@@ -7,4 +7,14 @@ public static class Day12
     public static int Part1() => 1;
 
     public static int Part2() => 2;
+
+    public static int Solve1(string[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int Solve2(string[] input)
+    {
+        throw new NotImplementedException();
+    }
 }
--- tests/aoc-2020-csharp-tests/Day12Tests.cs
+++ tests/aoc-2020-csharp-tests/Day12Tests.cs
@@ -5,17 +5,51 @@ namespace aoc_2020_csharp_tests;
 public class Day12Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "F10",
+            "N3",
+            "F7",
+            "R90",
+            "F11"
+        };
+
+        var expected = 25;
+        var actual = Day12.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2458;
         var actual = Day12.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "F10",
+            "N3",
+            "F7",
+            "R90",
+            "F11"
+        };
+
+        var expected = 286;
+        var actual = Day12.Solve2(input);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 145117;
         var actual = Day12.Part2();
         actual.Should().Be(expected);
     }
