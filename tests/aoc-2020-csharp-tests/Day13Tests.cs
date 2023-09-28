6e61a79233839f194b270ad9328a8005d94c99cd
Joe Nace <jwnace@gmail.com>
2023-09-28 11:12:56 -0400

    added tests for day 13

--- tests/aoc-2020-csharp-tests/Day13Tests.cs
+++ tests/aoc-2020-csharp-tests/Day13Tests.cs
@@ -5,17 +5,43 @@ namespace aoc_2020_csharp_tests;
 public class Day13Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input =
+            """
+            939
+            7,13,x,x,59,x,31,19
+            """;
+
+        var expected = 295;
+        var actual = Day13.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 2092;
         var actual = Day13.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [TestCase("7,13,x,x,59,x,31,19", 1068781)]
+    [TestCase("17,x,13,19", 3417)]
+    [TestCase("67,7,59,61", 754018)]
+    [TestCase("67,x,7,59,61", 779210)]
+    [TestCase("67,7,x,59,61", 1261476)]
+    [TestCase("1789,37,47,1889", 1202161486)]
+    public void Part2_Example_ReturnsCorrectAnswer(string input, long expected)
+    {
+        var actual = Day13.Solve2(input);
         actual.Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
+        var expected = 702970661767766;
         var actual = Day13.Part2();
         actual.Should().Be(expected);
     }
