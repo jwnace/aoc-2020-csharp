669deab3904526c4b37fc6b36ef65bf150d58216
Joe Nace <jwnace@gmail.com>
2023-09-29 09:25:05 -0400

    added tests for day 17 examples

--- src/aoc-2020-csharp/Day17/Day17.cs
+++ src/aoc-2020-csharp/Day17/Day17.cs
@@ -2,9 +2,19 @@
 
 public static class Day17
 {
-    private static readonly string Input = File.ReadAllText("Day17/day17.txt");
+    private static readonly string Input = File.ReadAllText("Day17/day17.txt").Trim();
 
-    public static int Part1() => 1;
+    public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
+
+    public static int Solve1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static int Solve2(string input)
+    {
+        throw new NotImplementedException();
+    }
 }
--- tests/aoc-2020-csharp-tests/Day17Tests.cs
+++ tests/aoc-2020-csharp-tests/Day17Tests.cs
@@ -5,10 +5,40 @@ namespace aoc_2020_csharp_tests;
 public class Day17Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input =
+            """
+            .#.
+            ..#
+            ###
+            """;
+
+        var expected = 112;
+        var actual = Day17.Solve1(input);
+        actual.Should().Be(expected);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
         var expected = 240;
         var actual = Day17.Part1();
+        actual.Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input =
+            """
+            .#.
+            ..#
+            ###
+            """;
+
+        var expected = 848;
+        var actual = Day17.Solve1(input);
         actual.Should().Be(expected);
     }
 
