f3c3858b001cd1c02abba54a0674ca74b04bb65f
Joe Nace <jwnace@gmail.com>
2023-09-29 11:39:21 -0400

    fixed formatting

--- src/aoc-2020-csharp/Day17/Day17.cs
+++ src/aoc-2020-csharp/Day17/Day17.cs
@@ -4,19 +4,19 @@ public static class Day17
 {
     private static readonly string Input = File.ReadAllText("Day17/day17.txt").Trim();
 
-public static int Part1() => Solve<Grid3d, Coordinate3d>(Input);
+    public static int Part1() => Solve<Grid3d, Coordinate3d>(Input);
 
-public static int Part2() => Solve<Grid4d, Coordinate4d>(Input);
+    public static int Part2() => Solve<Grid4d, Coordinate4d>(Input);
 
-public static int Solve<TGrid, TCoordinate>(string input) where TGrid : IGrid<TGrid, TCoordinate>
-{
-    var grid = TGrid.Parse(input);
-
-    for (var i = 0; i < 6; i++)
+    public static int Solve<TGrid, TCoordinate>(string input) where TGrid : IGrid<TGrid, TCoordinate>
     {
-        grid = grid.Step();
-    }
+        var grid = TGrid.Parse(input);
 
-    return grid.Count(x => x.Value);
-}
+        for (var i = 0; i < 6; i++)
+        {
+            grid = grid.Step();
+        }
+
+        return grid.Count(x => x.Value);
+    }
 }
