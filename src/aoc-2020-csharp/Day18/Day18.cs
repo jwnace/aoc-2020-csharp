c86bbb13dd944be83d85b5e994e1d5fc4bcdaad4
Joe Nace <jwnace@gmail.com>
2023-09-29 12:52:45 -0400

    added tests for day 18 examples

--- src/aoc-2020-csharp/Day18/Day18.cs
+++ src/aoc-2020-csharp/Day18/Day18.cs
@@ -2,9 +2,19 @@
 
 public static class Day18
 {
-    private static readonly string Input = File.ReadAllText("Day18/day18.txt");
+    private static readonly string[] Input = File.ReadAllLines("Day18/day18.txt");
 
-    public static long Part1() => 1;
+    public static long Part1() => Input.Sum(Solve1);
 
-    public static long Part2() => 2;
+    public static long Part2() => Input.Sum(Solve2);
+
+    public static long Solve1(string input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static long Solve2(string input)
+    {
+        throw new NotImplementedException();
+    }
 }
