26e12c91ef2a70428478d3604ccfae3bb7882d2f
Joe Nace <jwnace@gmail.com>
2023-10-21 14:31:13 -0400

    added solution for part 1

--- tests/aoc-2020-csharp-tests/Day19Tests.cs
+++ tests/aoc-2020-csharp-tests/Day19Tests.cs
@@ -5,9 +5,31 @@ namespace aoc_2020_csharp_tests;
 public class Day19Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input =
+            """
+            0: 4 1 5
+            1: 2 3 | 3 2
+            2: 4 4 | 5 5
+            3: 4 5 | 5 4
+            4: "a"
+            5: "b"
+
+            ababbb
+            bababa
+            abbbab
+            aaabbb
+            aaaabbb
+            """;
+
+        Day19.Solve1(input).Should().Be(2);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        Day19.Part1().Should().Be(0);
+        Day19.Part1().Should().Be(107);
     }
 
     [Test]
