7bee1efb68c259bf79f358a83f9a3cf5eeb9e4cb
Joe Nace <jwnace@gmail.com>
2023-10-21 15:41:35 -0400

    added solution for part 2

--- src/aoc-2020-csharp/Day19/Day19.cs
+++ src/aoc-2020-csharp/Day19/Day19.cs
@@ -9,7 +9,7 @@ public static class Day19
 
     public static int Part1() => Solve1(Input);
 
-    public static int Part2() => 2;
+    public static int Part2() => Solve2(Input);
 
     public static int Solve1(string input)
     {
@@ -17,6 +17,18 @@ public static class Day19
         var map = rules.Select(x => x.Split(": ")).ToDictionary(x => x[0], x => x[1]);
         var regex = new Regex($"^{BuildRegex(map, "0")}$");
 
+        return messages.Count(x => regex.IsMatch(x));
+    }
+
+    public static int Solve2(string input)
+    {
+        var (rules, messages) = input.Split("\n\n").Select(group => group.Split("\n")).ToArray();
+        var map = rules.Select(x => x.Split(": ")).ToDictionary(x => x[0], x => x[1]);
+
+        map["8"] = "42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42";
+        map["11"] = "42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31";
+
+        var regex = new Regex($"^{BuildRegex(map, "0")}$");
         return messages.Count(x => regex.IsMatch(x));
     }
 
