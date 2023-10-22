1ab47d9caaa64613abe37f4ff7734b4965a3d8dd
Joe Nace <jwnace@gmail.com>
2023-10-21 22:31:07 -0400

    updated tests and placeholder for day 21 solution

--- tests/aoc-2020-csharp-tests/Day21Tests.cs
+++ tests/aoc-2020-csharp-tests/Day21Tests.cs
@@ -5,18 +5,28 @@ namespace aoc_2020_csharp_tests;
 public class Day21Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[]
+        {
+            "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+            "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+            "sqjhc fvjkl (contains soy)",
+            "sqjhc mxmxvkd sbzzf (contains fish)",
+        };
+
+        Day21.Solve1(input).Should().Be(5);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day21.Part1();
-        actual.Should().Be(expected);
+        Day21.Part1().Should().Be(0);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day21.Part2();
-        actual.Should().Be(expected);
+        Day21.Part2().Should().Be(0);
     }
 }
