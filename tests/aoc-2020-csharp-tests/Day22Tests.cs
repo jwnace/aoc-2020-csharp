f9131429178d5e752303e57c7a23f4de09f6f8f5
Joe Nace <jwnace@gmail.com>
2023-10-22 12:38:00 -0400

    added solution for part 2

--- tests/aoc-2020-csharp-tests/Day22Tests.cs
+++ tests/aoc-2020-csharp-tests/Day22Tests.cs
@@ -34,8 +34,31 @@ public class Day22Tests
     }
 
     [Test]
+    public void Part2_Example_ReturnsCorrectAnswer()
+    {
+        var input =
+            """
+            Player 1:
+            9
+            2
+            6
+            3
+            1
+
+            Player 2:
+            5
+            8
+            4
+            7
+            10
+            """;
+
+        Day22.Solve2(input).Should().Be(291);
+    }
+
+    [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        Day22.Part2().Should().Be(0);
+        Day22.Part2().Should().Be(33455);
     }
 }
