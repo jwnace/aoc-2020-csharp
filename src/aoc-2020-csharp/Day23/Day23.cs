a2c01d72e92eda74cffc342984b5bac531a85a54
Joe Nace <jwnace@gmail.com>
2023-10-23 12:41:55 -0400

    added solution for part 1

--- src/aoc-2020-csharp/Day23/Day23.cs
+++ src/aoc-2020-csharp/Day23/Day23.cs
@@ -4,7 +4,51 @@ public static class Day23
 {
     private static readonly string Input = File.ReadAllText("Day23/day23.txt");
 
-    public static int Part1() => 1;
+    public static string Part1() => Solve1(Input, 100);
 
     public static int Part2() => 2;
+
+    public static string Solve1(string input, int moves)
+    {
+        var cups = input.Trim().Select(c => int.Parse(c.ToString())).ToArray();
+        var currentCup = cups[0];
+
+        for (var i = 0; i < moves; i++)
+        {
+            var currentCupIndex = Array.IndexOf(cups, currentCup);
+
+            if (currentCupIndex > cups.Length - 4)
+            {
+                cups = cups.Skip(3).Concat(cups.Take(3)).ToArray();
+                currentCupIndex = Array.IndexOf(cups, currentCup);
+            }
+
+            var pickedUpCups = cups.Skip(currentCupIndex + 1).Take(3).ToArray();
+            var remainingCups = cups.Take(currentCupIndex + 1).Concat(cups.Skip(currentCupIndex + 4)).ToArray();
+            var destinationCup = currentCup - 1;
+            while (pickedUpCups.Contains(destinationCup) || destinationCup < 1)
+            {
+                destinationCup--;
+                if (destinationCup < 1)
+                {
+                    destinationCup = cups.Max();
+                }
+            }
+
+            var destinationCupIndex = Array.IndexOf(remainingCups, destinationCup);
+            cups = remainingCups.Take(destinationCupIndex + 1).Concat(pickedUpCups)
+                .Concat(remainingCups.Skip(destinationCupIndex + 1)).ToArray();
+            currentCup = cups[(Array.IndexOf(cups, currentCup) + 1) % cups.Length];
+        }
+
+        var indexOf1 = Array.IndexOf(cups, 1);
+
+        return string.Join("", cups.Skip(indexOf1 + 1).Concat(cups.Take(indexOf1)));
+    }
+
+
+    public static long Solve2(string input, int moves)
+    {
+        throw new NotImplementedException();
+    }
 }
--- tests/aoc-2020-csharp-tests/Day23Tests.cs
+++ tests/aoc-2020-csharp-tests/Day23Tests.cs
@@ -4,19 +4,28 @@ namespace aoc_2020_csharp_tests;
 
 public class Day23Tests
 {
+    [TestCase("389125467", 10, "92658374")]
+    [TestCase("389125467", 100, "67384529")]
+    public void Part1_Example_ReturnsCorrectAnswer(string input, int moves, string expected)
+    {
+        Day23.Solve1(input, moves).Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day23.Part1();
-        actual.Should().Be(expected);
+        Day23.Part1().Should().Be("32897654");
+    }
+
+    [TestCase("389125467", 10_000_000, 149245887792)]
+    public void Part2_Example_ReturnsCorrectAnswer(string input, int moves, long expected)
+    {
+        Day23.Solve2(input, 10_000_000).Should().Be(expected);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day23.Part2();
-        actual.Should().Be(expected);
+        Day23.Part2().Should().Be(0);
     }
 }
