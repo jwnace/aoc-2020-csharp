004815e95a765d834bbc0657f4de456aca6ceace
Joe Nace <jwnace@gmail.com>
2023-10-23 16:40:27 -0400

    added solution for part 2

--- src/aoc-2020-csharp/Day25/Day25.cs
+++ src/aoc-2020-csharp/Day25/Day25.cs
@@ -1,10 +1,47 @@
+using aoc_2020_csharp.Extensions;
+
 namespace aoc_2020_csharp.Day25;
 
 public static class Day25
 {
-    private static readonly string Input = File.ReadAllText("Day25/day25.txt");
+    private static readonly int[] Input = File.ReadAllLines("Day25/day25.txt").Select(int.Parse).ToArray();
 
-    public static int Part1() => 1;
+    public static long Part1() => Solve1(Input);
 
     public static int Part2() => 2;
+
+    public static long Solve1(int[] input)
+    {
+        var (cardPublicKey, doorPublicKey) = input;
+
+        var cardLoopSize = FindLoopSize(cardPublicKey);
+
+        return Transform(doorPublicKey, cardLoopSize);
+    }
+
+    private static long FindLoopSize(int cardPublicKey)
+    {
+        var value = 1L;
+        var loopSize = 0L;
+
+        while (value != cardPublicKey)
+        {
+            value = value * 7 % 20201227;
+            loopSize++;
+        }
+
+        return loopSize;
+    }
+
+    private static long Transform(int doorPublicKey, long cardLoopSize)
+    {
+        var value = 1L;
+
+        for (var i = 0; i < cardLoopSize; i++)
+        {
+            value = value * doorPublicKey % 20201227;
+        }
+
+        return value;
+    }
 }
--- tests/aoc-2020-csharp-tests/Day25Tests.cs
+++ tests/aoc-2020-csharp-tests/Day25Tests.cs
@@ -5,9 +5,17 @@ namespace aoc_2020_csharp_tests;
 public class Day25Tests
 {
     [Test]
+    public void Part1_Example_ReturnsCorrectAnswer()
+    {
+        var input = new[] { 5764801, 17807724 };
+
+        Day25.Solve1(input).Should().Be(14897079);
+    }
+
+    [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        Day25.Part1().Should().Be(0);
+        Day25.Part1().Should().Be(19774660);
     }
 
     [Test]
