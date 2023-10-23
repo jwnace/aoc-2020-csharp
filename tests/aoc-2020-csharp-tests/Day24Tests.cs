07ffdb450bb3d9c0f4087ab74dd972507a80bc26
Joe Nace <jwnace@gmail.com>
2023-10-23 14:29:04 -0400

    added test for part 1 example

--- tests/aoc-2020-csharp-tests/Day24Tests.cs
+++ tests/aoc-2020-csharp-tests/Day24Tests.cs
@@ -4,19 +4,43 @@ namespace aoc_2020_csharp_tests;
 
 public class Day24Tests
 {
+    [TestCase(new[]
+    {
+        "sesenwnenenewseeswwswswwnenewsewsw",
+        "neeenesenwnwwswnenewnwwsewnenwseswesw",
+        "seswneswswsenwwnwse",
+        "nwnwneseeswswnenewneswwnewseswneseene",
+        "swweswneswnenwsewnwneneseenw",
+        "eesenwseswswnenwswnwnwsewwnwsene",
+        "sewnenenenesenwsewnenwwwse",
+        "wenwwweseeeweswwwnwwe",
+        "wsweesenenewnwwnwsenewsenwwsesesenwne",
+        "neeswseenwwswnwswswnw",
+        "nenwswwsewswnenenewsenwsenwnesesenew",
+        "enewnwewneswsewnwswenweswnenwsenwsw",
+        "sweneswneswneneenwnewenewwneswswnese",
+        "swwesenesewenwneswnwwneseswwne",
+        "enesenwswwswneneswsenwnewswseenwsese",
+        "wnwnesenesenenwwnenwsewesewsesesew",
+        "nenewswnwewswnenesenwnesewesw",
+        "eneswnwswnwsenenwnwnwwseeswneewsenese",
+        "neswnwewnwnwseenwseesewsenwsweewe",
+        "wseweeenwnesenwwwswnew",
+    }, 10)]
+    public void Part1_Example_ReturnsCorrectAnswer(string[] input, int expected)
+    {
+        Day24.Solve1(input).Should().Be(expected);
+    }
+
     [Test]
     public void Part1_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day24.Part1();
-        actual.Should().Be(expected);
+        Day24.Part1().Should().Be(0);
     }
 
     [Test]
     public void Part2_ReturnsCorrectAnswer()
     {
-        var expected = 0;
-        var actual = Day24.Part2();
-        actual.Should().Be(expected);
+        Day24.Part2().Should().Be(0);
     }
 }
